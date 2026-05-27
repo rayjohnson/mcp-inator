038a08ea73dae2cc4fc52515760db1a4af364657
Ray Johnson <ray.johnson@moov.io>
2026-05-27 10:39:04 -0700

    Fix lint: rename single-char test vars to lhs/rhs, fix comma spacing

--- mcp-inatorTests/Unit/MCPServerConfigEquatableTests.swift
+++ mcp-inatorTests/Unit/MCPServerConfigEquatableTests.swift
@@ -10,27 +10,27 @@ final class MCPServerConfigEquatableTests: XCTestCase {
     // error on every Apply for servers with non-alphabetical env var order.
 
     func testEnvVars_differentOrder_areEqual() {
-        var a = MCPServerConfig(displayName: "X", command: "/bin/x")
-        a.envVars = [EnvVar(key: "Z_KEY", value: "1"), EnvVar(key: "A_KEY", value: "2")]
-        var b = MCPServerConfig(displayName: "X", command: "/bin/x")
-        b.envVars = [EnvVar(key: "A_KEY", value: "2"), EnvVar(key: "Z_KEY", value: "1")]
-        XCTAssertEqual(a, b, "Configs with identical envVars in different order must be equal")
+        var lhs = MCPServerConfig(displayName: "X", command: "/bin/x")
+        lhs.envVars = [EnvVar(key: "Z_KEY", value: "1"), EnvVar(key: "A_KEY", value: "2")]
+        var rhs = MCPServerConfig(displayName: "X", command: "/bin/x")
+        rhs.envVars = [EnvVar(key: "A_KEY", value: "2"), EnvVar(key: "Z_KEY", value: "1")]
+        XCTAssertEqual(lhs, rhs, "Configs with identical envVars in different order must be equal")
     }
 
     func testEnvVars_differentValues_notEqual() {
-        var a = MCPServerConfig(displayName: "X", command: "/bin/x")
-        a.envVars = [EnvVar(key: "TOKEN", value: "abc")]
-        var b = MCPServerConfig(displayName: "X", command: "/bin/x")
-        b.envVars = [EnvVar(key: "TOKEN", value: "xyz")]
-        XCTAssertNotEqual(a, b)
+        var lhs = MCPServerConfig(displayName: "X", command: "/bin/x")
+        lhs.envVars = [EnvVar(key: "TOKEN", value: "abc")]
+        var rhs = MCPServerConfig(displayName: "X", command: "/bin/x")
+        rhs.envVars = [EnvVar(key: "TOKEN", value: "xyz")]
+        XCTAssertNotEqual(lhs, rhs)
     }
 
     func testEnvVars_differentKeys_notEqual() {
-        var a = MCPServerConfig(displayName: "X", command: "/bin/x")
-        a.envVars = [EnvVar(key: "FOO", value: "1")]
-        var b = MCPServerConfig(displayName: "X", command: "/bin/x")
-        b.envVars = [EnvVar(key: "BAR", value: "1")]
-        XCTAssertNotEqual(a, b)
+        var lhs = MCPServerConfig(displayName: "X", command: "/bin/x")
+        lhs.envVars = [EnvVar(key: "FOO", value: "1")]
+        var rhs = MCPServerConfig(displayName: "X", command: "/bin/x")
+        rhs.envVars = [EnvVar(key: "BAR", value: "1")]
+        XCTAssertNotEqual(lhs, rhs)
     }
 
     // MARK: - Identity fields excluded from comparison
@@ -38,46 +38,46 @@ final class MCPServerConfigEquatableTests: XCTestCase {
     // uuid, displayName, serverKey, notes, and timestamps must not affect equality.
 
     func testIdentityFields_notCompared() {
-        let a = MCPServerConfig(displayName: "Name A", serverKey: "key-a", command: "/bin/x")
-        let b = MCPServerConfig(displayName: "Name B", serverKey: "key-b", command: "/bin/x")
-        XCTAssertEqual(a, b)
+        let lhs = MCPServerConfig(displayName: "Name A", serverKey: "key-a", command: "/bin/x")
+        let rhs = MCPServerConfig(displayName: "Name B", serverKey: "key-b", command: "/bin/x")
+        XCTAssertEqual(lhs, rhs)
     }
 
     // MARK: - Agent-file fields do affect equality
 
     func testCommand_difference_notEqual() {
-        let a = MCPServerConfig(displayName: "X", command: "/bin/a")
-        let b = MCPServerConfig(displayName: "X", command: "/bin/b")
-        XCTAssertNotEqual(a, b)
+        let lhs = MCPServerConfig(displayName: "X", command: "/bin/a")
+        let rhs = MCPServerConfig(displayName: "X", command: "/bin/b")
+        XCTAssertNotEqual(lhs, rhs)
     }
 
     func testArgs_difference_notEqual() {
-        let a = MCPServerConfig(displayName: "X", command: "/bin/x", args: ["--foo"])
-        let b = MCPServerConfig(displayName: "X", command: "/bin/x", args: ["--bar"])
-        XCTAssertNotEqual(a, b)
+        let lhs = MCPServerConfig(displayName: "X", command: "/bin/x", args: ["--foo"])
+        let rhs = MCPServerConfig(displayName: "X", command: "/bin/x", args: ["--bar"])
+        XCTAssertNotEqual(lhs, rhs)
     }
 
     func testArgs_empty_vs_nonempty_notEqual() {
-        let a = MCPServerConfig(displayName: "X", command: "/bin/x")
-        let b = MCPServerConfig(displayName: "X", command: "/bin/x", args: ["--flag"])
-        XCTAssertNotEqual(a, b)
+        let lhs = MCPServerConfig(displayName: "X", command: "/bin/x")
+        let rhs = MCPServerConfig(displayName: "X", command: "/bin/x", args: ["--flag"])
+        XCTAssertNotEqual(lhs, rhs)
     }
 
     func testTransportType_difference_notEqual() {
-        let a = MCPServerConfig(displayName: "X", transportType: .http, url: "http://x.com")
-        let b = MCPServerConfig(displayName: "X", transportType: .sse,  url: "http://x.com")
-        XCTAssertNotEqual(a, b)
+        let lhs = MCPServerConfig(displayName: "X", transportType: .http, url: "http://x.com")
+        let rhs = MCPServerConfig(displayName: "X", transportType: .sse, url: "http://x.com")
+        XCTAssertNotEqual(lhs, rhs)
     }
 
     func testURL_difference_notEqual() {
-        let a = MCPServerConfig(displayName: "X", transportType: .http, url: "http://a.com")
-        let b = MCPServerConfig(displayName: "X", transportType: .http, url: "http://b.com")
-        XCTAssertNotEqual(a, b)
+        let lhs = MCPServerConfig(displayName: "X", transportType: .http, url: "http://a.com")
+        let rhs = MCPServerConfig(displayName: "X", transportType: .http, url: "http://b.com")
+        XCTAssertNotEqual(lhs, rhs)
     }
 
     func testStdioVsHTTP_notEqual() {
-        let a = MCPServerConfig(displayName: "X", command: "/bin/x")
-        let b = MCPServerConfig(displayName: "X", transportType: .http, url: "http://x.com")
-        XCTAssertNotEqual(a, b)
+        let lhs = MCPServerConfig(displayName: "X", command: "/bin/x")
+        let rhs = MCPServerConfig(displayName: "X", transportType: .http, url: "http://x.com")
+        XCTAssertNotEqual(lhs, rhs)
     }
 }
